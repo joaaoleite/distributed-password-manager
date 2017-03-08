271b06b17edb837271f86c5af2b6cae4f8dccf5d
joaaoleite <joaogsleite@gmail.com>
2017-03-08 14:35:17 +0000

    mvn with exec

--- sec/src/main/java/pt/ulisboa/tecnico/App.java
+++ sec/src/main/java/pt/ulisboa/tecnico/App.java
@@ -4,10 +4,10 @@ package pt.ulisboa.tecnico;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World: "+args[0] );
     }
 }
