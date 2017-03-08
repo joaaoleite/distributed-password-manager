c1239dbf40e7ba7a3defb56d19722d71c037ca0e
joaaoleite <joaogsleite@gmail.com>
2017-03-08 19:42:41 +0000

    client http api

--- client/src/main/java/pt/ulisboa/tecnico/sec/Client.java
+++ client/src/main/java/pt/ulisboa/tecnico/sec/Client.java
@@ -6,33 +6,6 @@ import org.json.JSONObject;
 
 public class Client{
 
-	private String endpoint;
-
-	public Client(String address, int port){
-		this.endpoint = "http://" + address + ":" + port + "/";
-	}
-
-	public void test(){
-		try {
-
-			JSONObject json = new JSONObject();
-			json.put("message", "Example message from client!");
-
-			String body = json.toString();
-
-			HttpResponse<JsonNode> request = Unirest.post(endpoint + "/test")
-				.header("accept", "application/json")
-				.body(body)
-				.asJson();
-			JSONObject response = request.getBody().getObject();
-			String msg = response.getString("message");
-			System.out.println("Response from server: "+msg);
-		}
-		catch(UnirestException e){
-			System.out.println("Request error!");
-		}
-	}
-
 	public static void main( String[] args ){
 
 		String address = args[0];
@@ -41,10 +14,8 @@ public class Client{
 		System.out.println("Creating client...");
 		System.out.println("Requests are going to "+address+":"+port);
 
-		Client client = new Client(address, port);
+		HttpApi httpApi = new HttpApi(address, port);
 
-		System.out.println("Making test request...");
-
-		client.test();
+		httpApi.register("abc123");
 	}
 }
