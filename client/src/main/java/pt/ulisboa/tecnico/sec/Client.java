6328e9bb150cad5e2f9dc24d52ff29519146858c
joaaoleite <joaogsleite@gmail.com>
2017-03-08 15:11:04 +0000

    project structure

---
+++ client/src/main/java/pt/ulisboa/tecnico/sec/Client.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.sec;
+
+/**
+ * Hello world!
+ *
+ */
+public class Client
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Client: " + args[0]);
+    }
+}
---
+++ server/src/main/java/pt/ulisboa/tecnico/sec/Server.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.sec;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Server: " + args[0]);
+    }
+}
