55b05e3e743f57a8f32d78c7fcfbea21901313d2
António Freire <tonifreire93@gmail.com>
2017-03-10 10:09:21 +0000

    starting api

---
+++ server/src/main/java/pt/ulisboa/tecnico/sec/Api.java
@@ -0,0 +1,17 @@
+package pt.ulisboa.tecnico.sec;
+
+
+
+public class Api
+{
+  public static void register(String publicKey) {
+   // body
+  }
+  public static void put(String publicKey, String domain,  String username, String password) {
+   // body
+  }
+  public static void get(String publicKey, String domain,  String username) {
+   // body
+  }
+
+}
