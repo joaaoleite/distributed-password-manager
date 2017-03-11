e505689a0a7aa9b00e59dd237d81c252eceb37e6
António Freire <tonifreire93@gmail.com>
2017-03-11 19:48:29 +0000

    add maccode to security paste

--- client/src/main/java/pt/ulisboa/tecnico/sec/security/MacCode.java
+++ client/src/main/java/pt/ulisboa/tecnico/sec/security/MacCode.java
@@ -8,10 +8,16 @@ import java.util.Arrays;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
 
 /** Generate a Message Authentication Code */
 public class MacCode {
   private SecretKey key;
+
+  public SecretKey getKey(){
+      return this.key;
+  }
+
 
   /** auxiliary method to generate SecretKey */
   public SecretKey generate() throws Exception {
@@ -24,27 +30,27 @@ public class MacCode {
   }
 
   /** auxiliary method to make the MAC */
-  public byte[] makeMAC(byte[] bytes, SecretKey key) throws Exception {
+  public String makeMAC(String text, SecretKey key) throws Exception {
 
     Mac cipher = Mac.getInstance("HmacMD5");
     cipher.init(key);
-    byte[] cipherDigest = cipher.doFinal(bytes);
+    byte[] cipherDigest = cipher.doFinal(text.getBytes());
 
     System.out.println("CipherDigest:");
     System.out.println(printHexBinary(cipherDigest));
 
-    return cipherDigest;
+    return DatatypeConverter.printBase64Binary(cipherDigest);
   }
 
   /** auxiliary method to calculate new digest from text and compare it to the
   to deciphered digest */
   public boolean verifyMAC(byte[] cipherDigest,
-  byte[] bytes,
+  String text,
   SecretKey key) throws Exception {
 
     Mac cipher = Mac.getInstance("HmacMD5");
     cipher.init(key);
-    byte[] cipheredBytes = cipher.doFinal(bytes);
+    byte[] cipheredBytes = cipher.doFinal(text.getBytes());
     return Arrays.equals(cipherDigest, cipheredBytes);
   }
 
--- client/src/main/java/pt/ulisboa/tecnico/sec/security/RSA.java
+++ client/src/main/java/pt/ulisboa/tecnico/sec/security/RSA.java
@@ -6,6 +6,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import javax.crypto.Cipher;
 import javax.xml.bind.DatatypeConverter;
+import java.security.SecureRandom;
 
 public class RSA {
 
@@ -13,7 +14,7 @@ public class RSA {
 	private PrivateKey privateKey;
 	private Cipher cipher;
 
-	public RSA(PublicKey publicKey, PrivateKey privateKey){
+	public RSA(PublicKey publicKey, PrivateKey privateKey)throws Exception{
 		this.publicKey = publicKey;
 		this.privateKey = privateKey;
 		this.cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
@@ -24,21 +25,23 @@ public class RSA {
 		this.privateKey = keys.getPrivate();
 	}
 
-	public String encrypt(String text){
-        cipher.init(Cipher.ENCRYPT_MODE, keys.getPublic());
-        byte[] cipherBytes = cipher.doFinal(plainText.getBytes());
+	public String encrypt(String text) throws Exception{
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] cipherBytes = cipher.doFinal(text.getBytes());
 		return DatatypeConverter.printBase64Binary(cipherBytes);
 	}
 
-	public String decrypt(String text){
+	public String decrypt(String text) throws Exception{
 		cipher.init(Cipher.DECRYPT_MODE, privateKey);
 		byte[] plainBytes = cipher.doFinal(DatatypeConverter.parseBase64Binary(text));
 		return new String(plainBytes);
 	}
 
-	public static KeyPair generateKeys(){
+	public static KeyPair generateKeys()throws Exception{
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(1024);
-        return keyGen.generateKeyPair();
+    SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+    keyGen.initialize(2048, random);
+    KeyPair pair = keyGen.generateKeyPair();
+    return pair;
 	}
 }
