44ad681ce5475c853470d580b8b3908c2e8f01a9
António Freire <tonifreire93@gmail.com>
2017-03-13 11:46:47 +0000

    mac is done

--- server/src/main/java/pt/ulisboa/tecnico/sec/Server.java
+++ server/src/main/java/pt/ulisboa/tecnico/sec/Server.java
@@ -2,15 +2,32 @@ package pt.ulisboa.tecnico.sec;
 import static spark.Spark.*;
 import org.json.JSONObject;
 import java.io.StringWriter;
+import io.jsonwebtoken.*;
+
+import io.jsonwebtoken.impl.crypto.MacProvider;
+import java.security.Key;
+import io.jsonwebtoken.impl.TextCodec;
+import javax.crypto.KeyGenerator;
 
 
 public class Server
 {
+
+
   public static void main( String[] args )
   {
+
     System.out.println( "Server address: http://localhost:" + args[0]);
     port(Integer.parseInt(args[0]));
     Routes routes = new Routes();
 
+
+  
+
+
+
   }
+
+
+
 }
