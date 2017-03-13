cfa2b4fa7d5b0ba05af278c8a5b017d12470d84f
joaotomazio <jdtomazio@gmail.com>
2017-03-13 12:49:56 +0000

    ensapsulation on keystore main

--- keystore/src/main/java/pt/ulisboa/tecnico/sec/Main.java
+++ keystore/src/main/java/pt/ulisboa/tecnico/sec/Main.java
@@ -1,10 +1,6 @@
 package pt.ulisboa.tecnico.sec;
 
-import java.io.File;
 import java.util.Scanner;
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.cert.X509Certificate;
 
 public class Main{
 
@@ -13,15 +9,8 @@ public class Main{
     System.out.print("Enter username: ");
     String username = reader.next();
     System.out.print("Enter password: ");
-    char[] password = reader.next().toCharArray();
+    String password = reader.next();
 
-    File dir = new File("data");
-    dir.mkdirs();
-
-    KeyPair pair = Generator.generateKeyPair();
-    PrivateKey privateKey = pair.getPrivate();
-    X509Certificate[] cert = Generator.generateCertificate(pair);
-
-    Generator.saveStore(username, password, privateKey, cert);
+    Generator.generateStore(username, password);
   }
 }
--- keystore/src/test/java/pt/ulisboa/tecnico/sec/KeyStoreTest.java
+++ keystore/src/test/java/pt/ulisboa/tecnico/sec/KeyStoreTest.java
@@ -1,17 +1,8 @@
 package pt.ulisboa.tecnico.sec;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.KeyStore;
-import java.io.File;
-import java.security.cert.X509Certificate;
-import java.security.cert.Certificate;
 /**
  * Unit test for simple App.
  */
@@ -19,29 +10,5 @@ public class KeyStoreTest extends TestCase
 {
     public void test() throws Exception{
 
-      File dir = new File("data");
-      dir.mkdirs();
-
-      KeyPair pair = Generator.generateKeyPair();
-      PrivateKey privateKey = pair.getPrivate();
-      PublicKey publicKey = pair.getPublic();
-      X509Certificate[] cert = Generator.generateCertificate(pair);
-
-      String username = "user";
-      char[] password = "pass".toCharArray();
-
-      Generator.saveStore(username, password, privateKey, cert);
-
-      FileInputStream fis = new FileInputStream("data/" + username + ".jce");
-      KeyStore ksa = KeyStore.getInstance("JCEKS");
-      ksa.load(fis, password);
-      fis.close();
-      PrivateKey k = (PrivateKey) ksa.getKey(username, password);
-      Certificate c = ksa.getCertificate(username);
-      PublicKey pk = c.getPublicKey();
-
-      assertEquals(privateKey, k);
-      assertEquals(cert[0], c);
-      assertEquals(publicKey, pk);
     }
 }
