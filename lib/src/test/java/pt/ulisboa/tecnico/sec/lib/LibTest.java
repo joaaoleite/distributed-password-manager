a25b4085a3d0581e66dc6d29902515b8a013445e
joaaoleite <joaogsleite@gmail.com>
2017-03-14 13:12:01 +0000

    LibTest AES

--- lib/src/test/java/pt/ulisboa/tecnico/sec/lib/LibTest.java
+++ lib/src/test/java/pt/ulisboa/tecnico/sec/lib/LibTest.java
@@ -4,9 +4,25 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import pt.ulisboa.tecnico.sec.lib.crypto.*;
+import pt.ulisboa.tecnico.sec.lib.http.*;
+import pt.ulisboa.tecnico.sec.lib.exceptions.*;
+
 public class LibTest extends TestCase {
 
 	public void test() throws Exception {
+
+		AES aes = new AES(AES.generateKey());
+		System.out.println("AES...");
+
+		String text = "gmail.com";
+		System.out.println("TEXT: "+text);
+
+		String encrypted = aes.encrypt(text);
+		System.out.println("ENCRYPTED: "+encrypted);
+
+		String decrypted = aes.decrypt(encrypted);
+		System.out.println("DECRYPTED: "+decrypted);
 
 	}
 }
