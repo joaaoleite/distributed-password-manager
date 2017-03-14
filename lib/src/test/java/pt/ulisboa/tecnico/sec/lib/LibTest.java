0203b82671b0d07e2e7e4a59713305a31eade932
joaaoleite <joaogsleite@gmail.com>
2017-03-14 13:05:03 +0000

    LibTest

---
+++ lib/src/test/java/pt/ulisboa/tecnico/sec/lib/LibTest.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.sec.lib;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class LibTest extends TestCase {
+
+	public void test() throws Exception {
+
+	}
+}
